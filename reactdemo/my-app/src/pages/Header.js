e4803da6d47f69b0840e6559c3c8064e962dc274
anfan <543042060@qq.com>
2018-08-21 10:08:11 +0800

    Save before ejecting

---
+++ reactdemo/my-app/src/pages/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './headercs.less'
+export class Header extends React.Component{
+    render(){
+        return(
+            <div>
+                你好啊
+            </div>
+        )
+    }
+}
